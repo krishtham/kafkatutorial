66f8f741334eb03c41d0f18fab95bb6d440a0038
Pradeep Thamanna <tkprao@hotmail.com>
2023-04-07 18:12:19 -0400

    Base Spring boot Application setup

---
+++ src/test/java/com/pradtham/sbootkafka/SpringBootKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradtham.sbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
